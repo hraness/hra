b688d89fecd0ed35c0ca8e208f30e515b24fa810
0thernet <ben@substrate.run>
2026-09-04 05:26:29 -0400

    chore(w2): re-pin installer digests after merging prose autorespond

--- src/install-normalizer.ts
+++ src/install-normalizer.ts
@@ -26,7 +26,7 @@ import { finished } from "node:stream/promises";
 import { createGunzip } from "node:zlib";
 
 export const HRA_INSTALL_BUN_VERSION = "1.3.14";
-export const HRA_INSTALL_CLI_SHA256 = "cae6dd49ba9bee8e31efe16cd2ade1cddd3628f9d4b9751ecfd3fa29e8ec607e";
+export const HRA_INSTALL_CLI_SHA256 = "cd9821e16819922b5379047e86a598ff30f6a5a493dbc0b7eedb78eed740ef89";
 
 const expectedPackageName = "@hraness/hra";
 const expectedPackageVersion = "0.3.0";
